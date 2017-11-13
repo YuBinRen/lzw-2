e6d928ab4e15104f5a01490cc831e3a15c4842cf
Sung-Kook Kim <skkeem@gmail.com>
2017-11-13 22:26:29 +0900

    final

--- lzw.py
+++ lzw.py
@@ -3,10 +3,11 @@
 from utils_lzw import Trie, Array
 import sys
 import math
+import time
+from subprocess import call
 from bitstring import BitArray, BitStream
 
-eFlag = False
-
+# encode string data into index array
 # string -> (size, index array)
 def lzw_encode(data):
     dic = Trie()
@@ -25,6 +26,7 @@ def lzw_encode(data):
     #print(dic.cnt) #
     return (dic.cnt, ret)
 
+# decode index array to string data
 # (size, index array) -> string
 def lzw_decode(dicSize, enc):
     dic = Array(dicSize)
@@ -52,6 +54,7 @@ def lzw_decode(dicSize, enc):
         i = i+1
     return ret
 
+# turn index array to bit array
 # (size, index array) -> BitArray
 def bitfy(dicSize, enc):
     ret = BitArray("uint:8=%s" % dicSize)
@@ -60,6 +63,7 @@ def bitfy(dicSize, enc):
         ret += BitArray("uint:%s=%s" % (bitSize, d))
     return ret
 
+# turn bit array to index array
 # BitArray -> (size, index array)
 def unbitfy(encoding):
     encoding = BitStream(encoding)
@@ -71,6 +75,12 @@ def unbitfy(encoding):
         ret.append(encoding.read("uint:%s" % bitSize))
     return (dicSize, ret)
 
+def print40(buf):
+    chars_per_line = 40
+    for i in range(0, len(buf), chars_per_line):
+        print(buf[i:i+chars_per_line])
+
+
 if __name__ == "__main__":
     # parse arguments
     import argparse
@@ -80,9 +90,6 @@ if __name__ == "__main__":
     parser.add_argument("outfile", type=str, help="decoded output file")
     parser.add_argument("-e", action="store_true", help="print on screen")
     args = parser.parse_args()
-
-    # set flags
-    eFlag = args.e
 
     # read input file
     data = None
@@ -94,9 +101,29 @@ if __name__ == "__main__":
     else:
         f.close()
 
+    # print input for encoding
+    if args.e:
+        print("## Input of Encoding:")
+        print('-' * 40)
+        print40(data)
+        print('-' * 40 + '\n')
+
     # lzw_encode
+    enc_start = time.time()
     dicSize, enc = lzw_encode(data)
     encoding = bitfy(dicSize, enc)
+    enc_end = time.time()
+    enc_size = len(encoding)
+
+    # print statistics for encoding
+    if args.e:
+        print("## Encoding Statistics:")
+        print('-' * 40)
+        print("# Encoding Time: %.8s secs" % (enc_end - enc_start))
+        print("# Encoded File Size: %s bits" % enc_size)
+        print("# Encoded File Size: %s bytes" % math.ceil(enc_size/8.0))
+        print("# Original File Size: %s bytes" % (len(data)))
+        print('-' * 40 + '\n')
 
     # write encoded output file
     try:
@@ -108,8 +135,30 @@ if __name__ == "__main__":
         f.close()
 
     # lzw_decode
+    dec_start = time.time()
     dicSize, enc = unbitfy(encoding)
     outfile = lzw_decode(dicSize, enc)
+    dec_end = time.time()
+
+    # print statistics for decoding
+    if args.e:
+        print("## Decoding Statistics:")
+        print('-' * 40)
+        print("# Decoding Time: %.8s secs" % (dec_end - dec_start))
+        print('-' * 40 + '\n')
+        print("## Result of Diff:")
+        print("## 'diff %s %s'" % (args.infile, args.outfile))
+        print('-' * 40)
+        if call(["diff", args.infile, args.outfile]) != 0:
+            print("!! Input and Output differs.")
+        print('-' * 40 + '\n')
+
+    # print output for decoding
+    if args.e:
+        print("## Output of Decoding:")
+        print('-' * 40)
+        print40(outfile)
+        print('-' * 40)
 
     # write decoded output file
     try:
